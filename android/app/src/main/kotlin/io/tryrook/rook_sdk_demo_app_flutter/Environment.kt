38389e5bd2d0650cc6ccbe65397fcbc8653ab59c
daniel-rook <daniel.nolasco@tryrook.io>
2024-05-14 10:22:41 -0600

    removed native auto sync implementation

--- android/app/src/main/kotlin/io/tryrook/rook_sdk_demo_app_flutter/Environment.kt
+++
@@ -1,8 +0,0 @@
-package io.tryrook.rook_sdk_demo_app_flutter
-
-import com.rookmotion.rook.sdk.domain.environment.RookEnvironment
-
-val rookEnvironment = if (BuildConfig.DEBUG) RookEnvironment.SANDBOX
-else RookEnvironment.PRODUCTION
-
-val isDebug = BuildConfig.DEBUG
--- android/app/src/main/kotlin/io/tryrook/rook_sdk_demo_app_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/io/tryrook/rook_sdk_demo_app_flutter/MainActivity.kt
@@ -1,27 +1,5 @@
 package io.tryrook.rook_sdk_demo_app_flutter
 
-import android.os.Build
-import android.os.Bundle
-import androidx.lifecycle.Lifecycle
-import com.rookmotion.rook.sdk.domain.enums.SyncInstruction
-import com.rookmotion.rook.sdk.framework.delegate.rookYesterdaySync
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-
-//    private val rookYesterdaySync by rookYesterdaySync(
-//        enableLogs = isDebug,
-//        clientUUID = REPLACE_WITH_YOUR_CLIENT_UUID,
-//        secretKey = REPLACE_WITH_YOUR_SECRET_KEY,
-//        environment = rookEnvironment,
-//    )
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Must be before super.onCreate(savedInstanceState)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//            rookYesterdaySync.enable(this)
-        }
-
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : FlutterActivity()
